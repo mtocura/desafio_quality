552c8b2dcbc04a64b291e16a3695a37d8791bdfb
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-15 15:52:14 -0300

    Refatorando nome de método e adicionando handler para exceção personalizada de bairro inexistente

--- src/main/java/br/com/quality/desafio_quality/exception/handler/ApiControllerExceptionAdvice.java
+++ src/main/java/br/com/quality/desafio_quality/exception/handler/ApiControllerExceptionAdvice.java
@@ -3,6 +3,7 @@ package br.com.quality.desafio_quality.exception.handler;
 import br.com.quality.desafio_quality.dto.ExceptionDTO;
 import br.com.quality.desafio_quality.dto.FieldExceptionDTO;
 import br.com.quality.desafio_quality.exception.HouseNotFoundException;
+import br.com.quality.desafio_quality.utils.exception.DistrictNotFoundException;
 import br.com.quality.desafio_quality.utils.exception.FieldErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +29,12 @@ public class ApiControllerExceptionAdvice {
     }
 
     @ExceptionHandler(HouseNotFoundException.class)
-    public ResponseEntity<?> houseNotExistsHandler(HouseNotFoundException e) {
+    public ResponseEntity<?> houseNotFoundHandler(HouseNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO(e.getMessage()));
     }
+    @ExceptionHandler(DistrictNotFoundException.class)
+    public ResponseEntity<?> districtNotFoundHandler(DistrictNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO(e.getMessage()));
+    }
+
 }
