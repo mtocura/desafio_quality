781bbb6784cf01d231f8e04fd72dddc53a0493f2
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-15 14:48:59 -0300

    Criação do DTO para District

---
+++ src/main/java/br/com/quality/desafio_quality/dto/DistrictDTO.java
@@ -0,0 +1,17 @@
+package br.com.quality.desafio_quality.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DistrictDTO {
+  private Long id;
+  @JsonProperty("value_m2")
+  private BigDecimal value;
+}
