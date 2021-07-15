34cd17edb29148108f2652fb0bcdab879dd31357
amaiate <adriana.maiate@mercadolivre.com>
2021-07-15 10:51:52 -0300

    Incluindo alterações no regex e incluindo NotNull nos Forms

--- src/main/java/br/com/quality/desafio_quality/form/DistrictForm.java
+++ src/main/java/br/com/quality/desafio_quality/form/DistrictForm.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
@@ -15,11 +16,14 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class DistrictForm {
 
+  @NotNull(message = "O nome do bairro não pode ser nulo.")
   @NotEmpty(message = "O nome do bairro não pode estar vazio.")
+  @Pattern(regexp = "^([A-Z]{1})([a-z]{1,})$", message = "O nome do bairro deve ser composto por letras.")
   @Size(max = 45, message = "O comprimento do bairro não pode exceder 45 caracteres.")
   @JsonProperty("prop_district")
   private String name;
 
+  @NotNull(message = "O valor não pode ser nulo.")
   @NotEmpty(message = "O campo não pode estar vazio.")
   @Size(max = 13, message = "O comprimento não pode exceder 13 digitos.")
   @JsonProperty("value_distric_m2")
--- src/main/java/br/com/quality/desafio_quality/form/HouseForm.java
+++ src/main/java/br/com/quality/desafio_quality/form/HouseForm.java
@@ -17,16 +17,19 @@ import java.util.List;
 @AllArgsConstructor
 public class HouseForm {
 
+  @NotNull(message = "O nome da propriedade não pode ser nulo.")
   @NotEmpty(message = "O nome da propriedade não pode estar vazio.")
   @Pattern(regexp = "^([A-Z]{1})([a-z]{1,})$", message = "O nome da propriedade deve começar com uma letra maiúscula.")
   @Size(max = 30, message = "O comprimento do nome não pode exceder 30 caracteres.")
   @JsonProperty("prop_name")
   private String name;
 
+  @NotNull(message = "O district não pode ser nulo.")
   @NotEmpty(message = "O district não pode ser vazio.")
   @Valid
   private DistrictForm district;
 
+  @NotNull(message = "Os rooms não pode ser nulos.")
   @NotEmpty(message = "Os rooms não podem ser vazios.")
   @Valid
   private List<RoomForm> rooms;
