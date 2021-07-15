20e9b427e06c79ae520a3106434f86f1b9bb6823
amaiate <adriana.maiate@mercadolivre.com>
2021-07-15 10:33:47 -0300

    Incluindo validações no Form DistrictForm

--- src/main/java/br/com/quality/desafio_quality/form/DistrictForm.java
+++ src/main/java/br/com/quality/desafio_quality/form/DistrictForm.java
@@ -5,14 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class DistrictForm {
+
+  @NotEmpty(message = "O nome do bairro não pode estar vazio.")
+  @Size(max = 45, message = "O comprimento do bairro não pode exceder 45 caracteres.")
   @JsonProperty("prop_district")
   private String name;
+
+  @NotEmpty(message = "O campo não pode estar vazio.")
+  @Size(max = 13, message = "O comprimento não pode exceder 13 digitos.")
   @JsonProperty("value_distric_m2")
   private BigDecimal value;
 }
