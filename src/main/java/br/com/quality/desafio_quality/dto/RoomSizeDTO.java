677cd530de7492531ecf23a8051fc02c10079b9f
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-15 14:47:31 -0300

    Refatorando nome de classe RoomDTO para RoomSizeDTO

--- src/main/java/br/com/quality/desafio_quality/dto/RoomSizeDTO.java
+++ src/main/java/br/com/quality/desafio_quality/dto/RoomSizeDTO.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class RoomDTO {
+public class RoomSizeDTO {
   @JsonProperty("room_name")
   private String name;
   @JsonProperty("room_m2")
