446e6caa440644043b6540c08eabb484a63b8a5b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-15 08:32:12 -0300

    Inicialização do projeto Spring Boot

---
+++ src/test/java/br/com/quality/desafio_quality/DesafioQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quality.desafio_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioQualityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
