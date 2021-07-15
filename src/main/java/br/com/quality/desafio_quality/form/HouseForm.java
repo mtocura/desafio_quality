1c2e2254f46d838b24eb300fda6b4fe0432b7b8d
amaiate <adriana.maiate@mercadolivre.com>
2021-07-15 10:34:44 -0300

    Incluindo validações no Form HouseForm

--- src/main/java/br/com/quality/desafio_quality/form/HouseForm.java
+++ src/main/java/br/com/quality/desafio_quality/form/HouseForm.java
@@ -5,14 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class HouseForm {
+
+  @NotEmpty(message = "O nome da propriedade não pode estar vazio.")
+  @Pattern(regexp = "^([A-Z]{1})([a-z]{1,})$", message = "O nome da propriedade deve começar com uma letra maiúscula.")
+  @Size(max = 30, message = "O comprimento do nome não pode exceder 30 caracteres.")
   @JsonProperty("prop_name")
   private String name;
+
+  @NotEmpty(message = "O district não pode ser vazio.")
+  @Valid
   private DistrictForm district;
+
+  @NotEmpty(message = "Os rooms não podem ser vazios.")
+  @Valid
   private List<RoomForm> rooms;
 }
