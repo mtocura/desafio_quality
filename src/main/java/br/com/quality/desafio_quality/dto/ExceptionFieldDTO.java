175217005bd3d27057deb65c1083bbe585c0fe82
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-15 10:25:49 -0300

    Criação do DTO para exceção lançada pelos validadores

--- src/main/java/br/com/quality/desafio_quality/dto/ExceptionFieldDTO.java
+++ src/main/java/br/com/quality/desafio_quality/dto/ExceptionFieldDTO.java
@@ -1,2 +1,19 @@
-package br.com.quality.desafio_quality.dto;public class ExceptionFieldDTO {
+package br.com.quality.desafio_quality.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
+public class ExceptionFieldDTO {
+
+    @JsonProperty("ValidationError")
+    private Map<String, String> validationErrors;
+
+    public ExceptionFieldDTO(Map<String, String> validationErrors) {
+        this.validationErrors = validationErrors;
+    }
+
+    public Map<String, String> getValidationErrors() {
+        return validationErrors;
+    }
 }
