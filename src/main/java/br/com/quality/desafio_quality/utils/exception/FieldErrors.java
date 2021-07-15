78969f4021de921b29584f3d86a61c074646f6e2
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-15 10:27:48 -0300

    Criação da classe FieldErros

--- src/main/java/br/com/quality/desafio_quality/utils/exception/FieldErrors.java
+++ src/main/java/br/com/quality/desafio_quality/utils/exception/FieldErrors.java
@@ -1,4 +1,18 @@
-package br.com.quality.desafio_quality.utils;
+package br.com.quality.desafio_quality.utils.exception;
+
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FieldErrors {
+
+    public static Map<String, String> processFieldErrors(List<FieldError> fieldErrors) {
+        Map<String, String> exceptions = new HashMap<>();
+        for (FieldError fieldError: fieldErrors) {
+            exceptions.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return exceptions;
+    }
 }
