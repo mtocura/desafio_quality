8e39ebca3bd5f785197a570779f20308f59169ad
amaiate <adriana.maiate@mercadolivre.com>
2021-07-15 11:38:23 -0300

    Incluindo service no controller e métodos post e get por Id e get all

--- src/main/java/br/com/quality/desafio_quality/controller/HouseController.java
+++ src/main/java/br/com/quality/desafio_quality/controller/HouseController.java
@@ -1,4 +1,38 @@
 package br.com.quality.desafio_quality.controller;
 
+import br.com.quality.desafio_quality.form.HouseForm;
+import br.com.quality.desafio_quality.service.HouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class HouseController {
+
+    private HouseService houseService;
+
+    public HouseController() {
+    }
+
+    @Autowired
+    public HouseController(HouseService houseService) {
+        this.houseService = houseService;
+    }
+
+    @PostMapping("/house")
+    public ResponseEntity<?> createHouse(@RequestBody HouseForm houseForm) {
+        return ResponseEntity.ok().body(this.houseService);
+    }
+
+
+    @GetMapping("/house")
+    public ResponseEntity<?> getAllHouses() {
+        return ResponseEntity.ok().body(this.houseService);
+    }
+
+    @GetMapping("/house/{id}")
+    public ResponseEntity<?> getHouseById(@PathVariable long id) {
+        return ResponseEntity.ok(this.houseService);
+    }
 }
