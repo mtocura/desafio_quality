035d547720eb056ca7227a962cbcbbb044017f76
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-15 14:54:33 -0300

    Adiconando converter para DTO

--- src/main/java/br/com/quality/desafio_quality/converter/RoomConverter.java
+++ src/main/java/br/com/quality/desafio_quality/converter/RoomConverter.java
@@ -1,5 +1,6 @@
 package br.com.quality.desafio_quality.converter;
 
+import br.com.quality.desafio_quality.dto.RoomDTO;
 import br.com.quality.desafio_quality.entity.Room;
 import br.com.quality.desafio_quality.form.RoomForm;
 import org.modelmapper.ModelMapper;
@@ -22,5 +23,19 @@ public class RoomConverter {
     }
 
     return rooms;
+  }
+
+  public static RoomDTO roomEntityToDTO(Room room) {
+    return mapper.map(room, RoomDTO.class);
+  }
+
+  public static List<RoomDTO> roomEntityToDTO(List<Room> rooms) {
+    List<RoomDTO> roomDTOS = new ArrayList<>();
+
+    for (Room room : rooms) {
+      roomDTOS.add(roomEntityToDTO(room));
+    }
+
+    return roomDTOS;
   }
 }
