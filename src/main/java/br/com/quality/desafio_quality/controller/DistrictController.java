d1f3877cf01ebad485562a6d835184e5c407deac
amaiate <adriana.maiate@mercadolivre.com>
2021-07-15 11:42:12 -0300

    Incluindo service no controller e métodos post e get por Id e get all

--- src/main/java/br/com/quality/desafio_quality/controller/DistrictController.java
+++ src/main/java/br/com/quality/desafio_quality/controller/DistrictController.java
@@ -1,4 +1,38 @@
 package br.com.quality.desafio_quality.controller;
 
+import br.com.quality.desafio_quality.form.DistrictForm;
+import br.com.quality.desafio_quality.service.DistrictService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class DistrictController {
+
+    private DistrictService districtService;
+
+    public DistrictController() {
+    }
+
+    @Autowired
+    public DistrictController(DistrictService districtService) {
+        this.districtService = districtService;
+    }
+
+    @PostMapping("/district")
+    public ResponseEntity<?> createDistrict(@RequestBody DistrictForm districtForm) {
+        return ResponseEntity.ok().body(this.districtService);
+    }
+
+
+    @GetMapping("/district")
+    public ResponseEntity<?> getAllDistricts() {
+        return ResponseEntity.ok().body(this.districtService);
+    }
+
+    @GetMapping("/district/{id}")
+    public ResponseEntity<?> getDistrictById(@PathVariable long id) {
+        return ResponseEntity.ok(this.districtService);
+    }
 }
