9d1e3f9ca8ce9e5a5cf7b06f51d1ce34d3caf090
João Lucas Nunes E Silva <joao.nunes@mercadolivre.com>
2021-07-16 09:54:59 -0300

    Remover service não utilizado e seus testes

--- src/main/java/br/com/quality/desafio_quality/controller/HouseController.java
+++ src/main/java/br/com/quality/desafio_quality/controller/HouseController.java
@@ -4,7 +4,6 @@ import br.com.quality.desafio_quality.converter.HouseConverter;
 import br.com.quality.desafio_quality.entity.House;
 import br.com.quality.desafio_quality.form.HouseForm;
 import br.com.quality.desafio_quality.service.HouseService;
-import br.com.quality.desafio_quality.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
