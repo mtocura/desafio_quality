f10b05319ef1e1a78906e0a0c64a041d8668bc1d
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-16 11:19:30 -0300

    Refatorando mensagem de erro

--- src/main/java/br/com/quality/desafio_quality/exception/handler/ApiControllerExceptionAdvice.java
+++ src/main/java/br/com/quality/desafio_quality/exception/handler/ApiControllerExceptionAdvice.java
@@ -41,7 +41,7 @@ public class ApiControllerExceptionAdvice {
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<?> emptyResultDataAccessHandler(EmptyResultDataAccessException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO("Distrito não encontrado"));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO(e.getMessage());
     }
 
 }
